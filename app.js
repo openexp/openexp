e5328a783498c5d54f1a9b32bfd0777e3d6b9960
andyh616 <andrew.heusser@gmail.com>
2015-11-20 16:23:25 -0500

    latest non functional version

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'mainController'
+]);
 
-myApp.config(function($routeProvider){
-
-  $routeProvider
-
-  .when('/', {
-    templateURL: 'pages/main.html',
-    controller: 'mainController'
-  })
-
-  .when('/sandbox', {
-    templateURL: 'pages/sandbox.html',
-    controller: 'sandboxController'
-  })
-
-  .when('/reactionTimeTask', {
-    templateURL: 'pages/reactionTimeTask.html',
-    controller: 'experimentsController'
-  })
-
-});
-
-myApp.controller('mainController',['$scope',function($scope){
-  $scope.test = 'test';
-}]);
+  myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+      })
+      .when('/sandbox', {
+        templateURL: 'pages/sandbox.html',
+        controller: 'mainController'
+      })
+      .when('/reactionTimeTask', {
+        templateURL: 'pages/reactionTimeTask.html',
+        controller: 'mainController'
+      })
+  }]);
