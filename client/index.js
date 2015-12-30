957012f9fdf720685fb56f5770135affe798f4d3
Austin <tashoecraft@gmail.com>
2015-12-30 10:56:57 -0500

    added ability to load styles

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 module.exports = function() {
     // APP
+    require('./app/app.scss');
     require('./app/app');
     require('./app/home/home.js');
     require('./app/home/home.controller.js');
@@ -7,12 +8,10 @@ module.exports = function() {
     require('./app/connect/connect.controller.js');
     require('./app/dashboard/dashboard.js');
     require('./app/dashboard/dashboard.controller.js');
-    // Experiments. Maybe find a way to parse the folder with pattern
     require('./app/experiments/reactionTimeTask/reactionTimeTask.js');
     require('./app/experiments/reactionTimeTask/reactionTimeTask.controller.js');
     require('./app/experiments/triggerTest/triggerTest.js');
     require('./app/experiments/triggerTest/triggerTest.controller.js');
-
 
     // COMPONENTS
     require('./components/navbar/navbar.js');
