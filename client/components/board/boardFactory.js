91299338aef075704333844a411561036534bd5d
andyh616 <andrew.heusser@gmail.com>
2016-01-24 09:18:13 -0500

    ADD auto-connect to connect page
    When the user presses the connect button, the app will first try to
    auto connect to the device, and if that doesn’t work, it’ll return an
    array of available devices that will be automatically displayed to the
    screen.
    
    This update relies on a listPorts method that I added to the
    openbci-sdk.

--- client/components/board/boardFactory.js
+++ client/components/board/boardFactory.js
@@ -3,27 +3,48 @@ angular.module('OpenEXP')
         const OpenBCIBoard = require('openbci-sdk');
         const ourBoard = new OpenBCIBoard.OpenBCIBoard();
 
-        var findPorts = () => {
-            return new Promise((res, rej) => {
-                ourBoard.autoFindOpenBCIBoard((portName, ports) => {
-                    if(portName) {
-                        res(connect());
-                    } else {
-                        res(ports);
-                    }
-                })
-            });
+        // wrote a method in the openbci-sdk to return an array of ports and
+        // wrapping it in a promise here
+        var listDevices = () => {
+            return new Promise((res,rej) => {
+              ourBoard.listPorts().then((value) => {
+                res(value)
+                console.log(value)
+              })
+            })
         };
+
+        // when the connect state is loaded in the app, it auto runs this function
+        // which either connects to the board and starts the data stream, or
+        // returns an array of available devices
+        var tryAutoConnect = () => {
+            return new Promise((res, rej) => {
+              ourBoard.autoFindOpenBCIBoard().then((value) => {
+                if(Array.isArray(value)) {
+                  /**Unable to auto find OpenBCI board*/
+                  res(value);
+                  console.log("Board not auto-detected. Returning list of ports.")
+                } else {
+                  console.log("Auto-detected board!  Connecting...")
+                  res(connect(value))
+                }
+              })
+          })
+        };
+
         var storage = {};
 
         var connect = (portName) => {
             ourBoard.connect(portName)
                 .then(boardSerial => {
                     ourBoard.on('ready',function() {
-                        /** Start streaming, reading registers, what ever your heart desires  */
+                        console.log('Connected!')
+                        ourBoard.streamStart();
+                        this.isConnected = true;
                     });
                 }).catch(function(err) {
                 /** Handle connection errors */
+                console.log("error!")
             });
         };
 
@@ -39,7 +60,8 @@ angular.module('OpenEXP')
 
 
         return {
-            findPorts: findPorts,
+            tryAutoConnect: tryAutoConnect,
+            listDevices: listDevices,
             connect: connect,
             publish: publish,
             unpublish: unpublish
