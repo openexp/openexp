17dc3601d7f62fd672cc9fbd28bbca047ae88788
andyh616 <andrew.heusser@gmail.com>
2016-02-17 12:11:45 -0500

    last fix for this PR
    added back the urls for the local experiments

--- client/app/experiments/local/triggerTest/triggerTest.js
+++ client/app/experiments/local/triggerTest/triggerTest.js
@@ -2,6 +2,7 @@ angular.module('OpenEXP')
     .config($stateProvider => {
         $stateProvider
             .state('triggerTest', {
+                url: '/experiments/local/triggertest'
                 controller: 'TriggerCtrl',
                 templateUrl: './app/experiments/local/triggerTest/triggerTest.html'
         })
