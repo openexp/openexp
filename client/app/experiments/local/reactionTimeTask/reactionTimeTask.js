66a6939508dc2ddd728ff69fc37daa2c4732bfde
andyh616 <andrew.heusser@gmail.com>
2016-02-18 09:26:24 -0500

    FIX commas
    forgot some commas :)

--- client/app/experiments/local/reactionTimeTask/reactionTimeTask.js
+++ client/app/experiments/local/reactionTimeTask/reactionTimeTask.js
@@ -2,7 +2,7 @@ angular.module('OpenEXP')
     .config($stateProvider => {
         $stateProvider
             .state('reactionTimeTask', {
-                url: '/experiments/local/reactiontimetask'
+                url: '/experiments/local/reactiontimetask',
                 controller: 'ReactionCtrl',
                 templateUrl: './app/experiments/local/reactionTimeTask/reactionTimeTask.html'
         })
--- client/app/experiments/local/triggerTest/triggerTest.js
+++ client/app/experiments/local/triggerTest/triggerTest.js
@@ -2,7 +2,7 @@ angular.module('OpenEXP')
     .config($stateProvider => {
         $stateProvider
             .state('triggerTest', {
-                url: '/experiments/local/triggertest'
+                url: '/experiments/local/triggertest',
                 controller: 'TriggerCtrl',
                 templateUrl: './app/experiments/local/triggerTest/triggerTest.html'
         })
